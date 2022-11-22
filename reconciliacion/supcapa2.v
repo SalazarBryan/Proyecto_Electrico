2e3d61a2073c90011a91f54d00572795515683bf
ChrisFernandezVivas <christopher.fernandez@ucr.ac.cr>
2022-11-22 15:04:15 -0600

    hola

--- reconciliacion/supcapa2.v
+++ reconciliacion/supcapa2.v
@@ -1,13 +1,15 @@
-module signal_indication(
+module supcapa2(
     input clk,
     input col,
     output reg pls_signal_indication 
 );
+
+//empezamos con la lógica
 always @(posedge clk) begin
     if (!col)
     pls_signal_indication = 0;
     else
     pls_signal_indication = 1;
-end
+end//fin del always
 
 endmodule
