eca5236f18ce7b38d72799a690749d34701f372f
ChrisFernandezVivas <christopher.fernandez@ucr.ac.cr>
2022-11-22 15:05:39 -0600

    subcapa4

--- reconciliacion/supcapa4.v
+++ reconciliacion/supcapa4.v
@@ -3,11 +3,12 @@ module carrier_indication(
     input crs,
     output reg pls_carrier_indication
 );
+//Empezamos  con la lógica
 always @(posedge clk) begin
     if (crs) begin
         pls_carrier_indication = 1;
     end
     else
     pls_carrier_indication = 0;
-end
+end// fin del always
 endmodule
